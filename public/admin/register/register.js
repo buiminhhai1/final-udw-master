00d3868a508d2be8af8beb0cb8be4c42e6a96dab
Pedemanggiaydo <dada8028@gmail.com>
2019-06-15 12:05:58 +0700

    Fix error: not have password field

--- public/admin/register/register.js
+++ public/admin/register/register.js
@@ -5,26 +5,31 @@ function validform() {
         var c = document.forms["my-form"]["username"].value;
         var d = document.forms["my-form"]["permanent-address"].value;
         var e = document.forms["my-form"]["nid-number"].value;
+		var f = document.forms["my-form"]["Password"].value;
 
         if (a==null || a=="")
         {
-            alert("Please Enter Your Full Name");
+            alert("Vui lòng nhập họ tên");
             return false;
         }else if (b==null || b=="")
         {
-            alert("Please Enter Your Email Address");
+            alert("Vui lòng nhập địa chỉ Email");
             return false;
         }else if (c==null || c=="")
         {
-            alert("Please Enter Your Username");
+            alert("Vui lòng nhập tên đăng nhập");
+            return false;
+        }else if (f==null || f=="")
+        {
+            alert("Vui lòng nhập mật khẩu");
             return false;
         }else if (d==null || d=="")
         {
-            alert("Please Enter Your Permanent Address");
+            alert("Vui lòng nhập quê quán");
             return false;
         }else if (e==null || e=="")
         {
-            alert("Please Enter Your NID Number");
+            alert("Vui lòng nhập CMND");
             return false;
         }
 
